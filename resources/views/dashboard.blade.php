57c8b5ad640ab7936fc76b68475133069735022e
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2024-02-02 11:06:08 -0500

    ajustes dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,6 +51,34 @@
 
         <div class="container items-center px-4 py-8 m-auto mt-5">
             <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
+
+            <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $cantidadParticipantes }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total Inscritos</p>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 dark:text-gray-200"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                                clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        </div>
+                    </div>
+                </div>
             
                 <div class="w-full p-2 lg:w-1/4 md:w-1/2">
                     <div
@@ -59,12 +87,7 @@
                             <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
                                 fill="currentColor">
-                                <path fill-rule="evenodd"
-                                    d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-                                    clip-rule="evenodd" />
-                                <path fill-rule="evenodd"
-                                    d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-                                    clip-rule="evenodd" />
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </div>
@@ -82,7 +105,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                
+                <!-- <div class="w-full p-2 lg:w-1/4 md:w-1/2">
                     <div
                         class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
                         <div class="flex flex-row justify-between items-center">
@@ -94,7 +118,7 @@
                         </div>
                         
                         </div>
-                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$countInscripcion}}</h1>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$cantidadParticipantes}}</h1>
                         <div class="flex flex-row justify-between dark:text-gray-200">
                         <p>Total Inscritos</p>
                         <span>
@@ -107,62 +131,62 @@
                         </span>
                         </div>
                     </div>
-                </div>
+                </div> -->
+               
                 <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-                <div
-                    class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
-                    <div class="flex flex-row justify-between items-center">
-                    <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
-                        <path fill-rule="evenodd"
-                            d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                            clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                   
-                    </div>
-                    <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$countPosMatricula}}</h1>
-                    <div class="flex flex-row justify-between dark:text-gray-200">
-                    <p>En estado de Pos-Matricula</p>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 dark:text-gray-200"
-                        viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd" />
-                        </svg>
-                    </span>
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countPreMatricula + $countPosMatricula }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>En estado de Pre-matricula</p>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 dark:text-gray-200"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                                clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        </div>
                     </div>
                 </div>
-                </div>
+
                 <div class="w-full p-2 lg:w-1/4 md:w-1/2">
-                <div
-                    class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
-                    <div class="flex flex-row justify-between items-center">
-                    <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
-                        </svg>
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countInscripcion }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Pendientes por realizar pruebas</p>
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 dark:text-gray-200"
+                            viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd"
+                                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                                clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                        </div>
                     </div>
-                    
-                    </div>
-                    <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countPreMatricula }}
-                    </h1>
-                    <div class="flex flex-row justify-between dark:text-gray-200">
-                    <p>En estado de Pre-matricula</p>
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 dark:text-gray-200"
-                        viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd" />
-                        </svg>
-                    </span>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>

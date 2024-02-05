ad1a1805703e71a5867c86d6baa79d5ac9dc667c
Juan Esteban Londoño Gomez <72523178+Estebanxz11@users.noreply.github.com>
2024-02-05 18:00:09 -0500

    dashboar cauca y nariño

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,11 +19,33 @@ class DashboardController extends Controller
         $countPreMatricula = Participante::where('estado_registro', $estadoPreMatricula)->count();
         $cantidadParticipantes = Participante::count();
 
+        // Contar participantes del departamento Cauca
+        $countCauca = Participante::where('departamento_residencia', 'Cauca')->count();
+        $countCaucaPreMatriculados = Participante::where('estado_registro', $estadoPreMatricula)->where('departamento_residencia', 'Cauca')->count();
+        $countCaucaPosMatriculados = Participante::where('estado_registro', $estadoPosMatricula)->where('departamento_residencia', 'Cauca')->count();
+        $countCaucaMatriculados = Participante::where('estado_registro', $estadoMatricula)->where('departamento_residencia', 'Cauca')->count();
+
+
+        // Contar participantes del departamento Nariño
+        $countNariño = Participante::where('departamento_residencia', 'Nariño')->count();
+        $countNariñoPreMatriculados = Participante::where('estado_registro', $estadoPreMatricula)->where('departamento_residencia', 'Nariño')->count();
+        $countNariñoPosMatriculados = Participante::where('estado_registro', $estadoPosMatricula)->where('departamento_residencia', 'Nariño')->count();
+        $countNariñoMatriculados = Participante::where('estado_registro', $estadoMatricula)->where('departamento_residencia', 'Nariño')->count();
+
         $labels = ['Etiqueta 1', 'Etiqueta 2', 'Etiqueta 3'];
         $data = [30, 50, 20];
         
         // dd($estadoMatricula);
         return view('dashboard', ['countMatricula' => $countMatricula, 'countInscripcion' => $countInscripcion, 'countPosMatricula' => $countPosMatricula, 'countPreMatricula' =>  $countPreMatricula,
-    'labels' => $labels, 'data' => $data, 'cantidadParticipantes' => $cantidadParticipantes,]);
+        'labels' => $labels, 'data' => $data, 'cantidadParticipantes' => $cantidadParticipantes,
+        'countCauca' => $countCauca,
+        'countCaucaPreMatriculados' => $countCaucaPreMatriculados,
+        'countCaucaPosMatriculados' => $countCaucaPosMatriculados,
+        'countCaucaMatriculados' => $countCaucaMatriculados,
+        'countNariño' => $countNariño,
+        'countNariñoPreMatriculados' => $countNariñoPreMatriculados,
+        'countNariñoPosMatriculados' => $countNariñoPosMatriculados,
+        'countNariñoMatriculados' => $countNariñoMatriculados,
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,47 +9,9 @@
     
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
        
-        <!-- <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4 gap-4">
-          <div class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">{{ $countMatricula }}</p>
-              <p>Matriculados</p>
-            </div>
-          </div>
-          <div class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">{{ $countInscripcion }}</p>
-              <p>Inscritos</p>
-            </div>
-          </div>
-          <div class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">$11,257</p>
-              <p>Sales</p>
-            </div>
-          </div>
-          <div class="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
-            <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            </div>
-            <div class="text-right">
-              <p class="text-2xl">$75,257</p>
-              <p>Balances</p>
-            </div>
-          </div>
-        </div> -->
         <!-- ./Statistics Cards -->
 
-        <div class="container items-center px-4 py-8 m-auto mt-5">
+        <div class="container items-center px-4 m-auto mt-5">
             <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
 
             <div class="w-full p-2 lg:w-1/4 md:w-1/2">
@@ -72,6 +34,38 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        
+        <div class="container items-center px-4  m-auto ">
+
+            <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+                <h2 class="text-3xl font-semibold leading-tight">
+                    {{ __('Cauca') }}
+                </h2>
+            </div>
+            <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
+            <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countCauca }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total Inscritos</p>
+                        
+                        </div>
+                    </div>
+                </div>
             
                 <div class="w-full p-2 lg:w-1/4 md:w-1/2">
                     <div
@@ -84,9 +78,9 @@
                                 </svg>
                             </div>
                         </div>
-                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$countMatricula}}</h1>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$countCaucaPreMatriculados + $countCaucaPosMatriculados }}</h1>
                         <div class="flex flex-row justify-between dark:text-gray-200">
-                        <p>Total Matriculados</p>
+                        <p>Total Pre-matriculados</p>
                         
                         </div>
                     </div>
@@ -104,10 +98,10 @@
                         </div>
                         
                         </div>
-                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countPreMatricula + $countPosMatricula }}
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countCaucaMatriculados }}
                         </h1>
                         <div class="flex flex-row justify-between dark:text-gray-200">
-                        <p>En estado de Pre-matricula</p>
+                        <p>Total Matriculados</p>
                         
                         </div>
                     </div>
@@ -125,16 +119,111 @@
                         </div>
                         
                         </div>
-                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countInscripcion }}
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ 10 }}
                         </h1>
                         <div class="flex flex-row justify-between dark:text-gray-200">
-                        <p>Pendientes por prueba</p>
+                        <p>Total cohorte 1</p>
                         
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="container items-center px-4  m-auto ">
+
+            <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
+                <h2 class="text-3xl font-semibold leading-tight">
+                    {{ __('Nariño') }}
+                </h2>
+            </div>
+            <div class="flex flex-wrap pb-3 mx-4 md:mx-24 lg:mx-0">
+            <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countNariño }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total Inscritos</p>
+                        
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                            <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{$countNariñoPreMatriculados + $countNariñoPosMatriculados }}</h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total Pre-matriculados</p>
+                        
+                        </div>
+                    </div>
+                </div>
+               
+                <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ $countNariñoMatriculados }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total Matriculados</p>
+                        
+                        </div>
+                    </div>
+                </div>
+
+                <div class="w-full p-2 lg:w-1/4 md:w-1/2">
+                    <div
+                        class="flex flex-col px-6 py-10 overflow-hidden bg-white dark:bg-gradient-to-br dark:from-purple-400 dark:via-blue-400 dark:to-blue-500 rounded-xl shadow-lg duration-300 dark:shadow-2xl group">
+                        <div class="flex flex-row justify-between items-center">
+                        <div class="px-4 py-4 bg-gray-300  rounded-xl bg-opacity-30">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 dark:text-gray-50" viewBox="0 0 20 20"
+                            fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        
+                        </div>
+                        <h1 class="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-700 mt-12 dark:text-gray-50">{{ 10 }}
+                        </h1>
+                        <div class="flex flex-row justify-between dark:text-gray-200">
+                        <p>Total cohorte 1</p>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</div>
     </div>
     
     <!-- <div class="container mx-auto mt-8">
